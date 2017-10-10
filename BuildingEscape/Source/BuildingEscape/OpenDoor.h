bc6586dc5e2b8f58441092ad28a3377eb6eb2575
Lee Anderson <leeander10@gmail.com>
2017-10-09 22:25:59 -0500

    Allow rotation of door to be controlled by blueprint

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -7,6 +7,7 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -25,6 +26,9 @@ protected:
 
 	void OpenDoor();
 	void CloseDoor();
+
+	UPROPERTY(BluePrintAssignable)
+	FOnOpenRequest OnOpenRequest;
 
 public:	
 	// Called every frame
